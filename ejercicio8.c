98a5b4df6fc131e48793f015b219be311f45e0cb
monicacice <manzanolomonica@gmail.com>
2020-01-28 22:00:37 +0100

    adivino tocho

--- ejercicio8.c
+++ ejercicio8.c
@@ -4,33 +4,41 @@
 
 #define MAX 10
 #define NUMERO_INTENTOS 5
+#define PARTIDAS 3
 int getRandom();
 int main(int argc, char const *argv[])
 {
-    //Obtener el numero aleatorio. DOS == comparas. CON = estás asignando un valor.
-
-    int numeroAleatorio = getRandom();
-    //printf("%d", numeroAleatorio);
-
+    int numeroAleatorio;
     int numero;
-    for (int i = 0; i < NUMERO_INTENTOS; i++) //1.Inicializacion. 2. Validacion. 3. Incremento(que quieras)
+    //Obtener el numero aleatorio. DOS == comparas. CON = estás asignando un valor.
+    for (int i = 0; i < PARTIDAS; i++)
     {
-        printf("Introduce tu numero: ");
-        scanf("%d", &numero);
+        numeroAleatorio = getRandom();
+        printf("%d", numeroAleatorio);
+
+        for (int i = 0; i < NUMERO_INTENTOS; i++) //1.Inicializacion. 2. Validacion. 3. Incremento(que quieras)
+        {
+            printf("Introduce tu numero: ");
+            scanf("%d", &numero);
+            if (numero == numeroAleatorio)
+            {
+                printf("TOMA TU MEDALLA\n");
+                break;
+            }
+            else
+            {
+                printf("Ups...\n");
+            }
+        }
         if (numero == numeroAleatorio)
         {
-            printf("TOMA TU MEDALLA\n");
+            printf("VICTORIA :D\n");
             break;
         }
         else
         {
-            printf("Ups...\n");
+            printf("GAME OVER D:\n");
         }
-    }
-    if (numero == numeroAleatorio){
-        printf("VICTORIA :D\n");
-    } else {
-        printf("GAME OVER D:\n");
     }
 }
 
